54a27a38f36c3444efa375e80cb1741e6efca3c5
Magic <meilunzhi@gmail.com>
2023-04-28 17:50:20 +0800

    bugfix

--- resources/configs/vue.config.js
+++ resources/configs/vue.config.js
@@ -34,6 +34,7 @@ module.exports = {
   lintOnSave: process.env.NODE_ENV === 'development',
   productionSourceMap: false,
   devServer: {
+    clientLogLevel: 'debug', // 调试
     port: port,
     open: true,
     overlay: {
